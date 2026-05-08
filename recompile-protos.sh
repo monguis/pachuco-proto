b55103c2f0370b997b7bec3f54a31a7d42af65e9
unknown <jccnnetwork@gmail.com>
2026-05-07 20:50:22 -0600

    updated user body

--- recompile-protos.sh
+++ recompile-protos.sh
@@ -1,11 +1,10 @@
 protoc \
- --go_out=. --go_opt=paths=source_relative \
- --go-grpc_out=. --go-grpc_opt=paths=source_relative \
- --grpc-gateway_out=. --grpc-gateway_opt paths=source_relative \
+ --go_out=../proto --go_opt=paths=source_relative \
+ --go-grpc_out=../proto --go-grpc_opt=paths=source_relative \
+ --grpc-gateway_out=../proto --grpc-gateway_opt paths=source_relative \
  dice-throws_protos/dice-throws.proto
 
-protoc \
- --go_out=. --go_opt=paths=source_relative \
- --go-grpc_out=. --go-grpc_opt=paths=source_relative \
- --grpc-gateway_out=. --grpc-gateway_opt paths=source_relative \
- user_protos/users.proto
+protoc --go_out=../proto \
+ --go_opt=paths=source_relative \
+ --go-grpc_out=../proto  --go-grpc_opt=paths=source_relative \
+ --grpc-gateway_out=../proto  --grpc-gateway_opt paths=source_relative user_protos/users.proto
