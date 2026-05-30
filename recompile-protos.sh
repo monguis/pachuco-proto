9d454383531c0a140917a57d2645476af0504461
unknown <jccnnetwork@gmail.com>
2026-05-30 12:55:24 -0600

    added game proto

--- recompile-protos.sh
+++ recompile-protos.sh
@@ -8,3 +8,8 @@ protoc --go_out=../proto \
  --go_opt=paths=source_relative \
  --go-grpc_out=../proto  --go-grpc_opt=paths=source_relative \
  --grpc-gateway_out=../proto  --grpc-gateway_opt paths=source_relative user_protos/users.proto
+
+protoc --go_out=../proto \
+ --go_opt=paths=source_relative \
+ --go-grpc_out=../proto  --go-grpc_opt=paths=source_relative \
+ --grpc-gateway_out=../proto  --grpc-gateway_opt paths=source_relative game_protos/games.proto
